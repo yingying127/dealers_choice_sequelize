3c4ba3753346efadf8c3cddcd561dfba5e7fc8d2
Ying Ying <yingyingfengg@gmail.com>
2022-02-15 18:35:07 -0500

    added data

--- server.js
+++ server.js
@@ -1,8 +1,49 @@
-const db = require('./db')
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/marvel_db');
+const { STRING } = require('sequelize');
 
-const init = () => {
+const Character = sequelize.define('character', {
+    name: {
+        type: STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        }
+    }
+});
+
+const Life = sequelize.define('life', {
+    name: {
+        type: STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        }
+    }
+});
+
+Character.belongsTo(Life);
+Life.hasMany(Character);
+
+const express = require('express');
+const { prototype } = require('pg/lib/connection-parameters');
+const app = express();
+
+const init = async() => {
     try {
+        await sequelize.sync({ force: true})
         console.log('starting!')
+        const alive = await Life.create({ name: 'alive'});
+        const decease = await Life.create({ name: 'decease'});
+        await Character.create({ name: 'Iron Man', lifeId: decease.id});
+        await Character.create({ name: 'Pietro Maximoff', lifeId: decease.id});
+        await Character.create({ name: 'Natasha Romanoff', lifeId: decease.id});
+        await Character.create({ name: 'Captain America', lifeId: alive.id});
+        await Character.create({ name: 'Doctor Strange', lifeId: alive.id});
+        await Character.create({ name: 'Clint Barton', lifeId: alive.id});
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => console.log(`listening on port ${port}`))
     }
     catch(ex) {
         console.log(ex)
