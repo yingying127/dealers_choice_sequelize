dbdc480c339f9c6f3a95b10b944d030660ba07af
Ying Ying <yingyingfengg@gmail.com>
2022-02-16 22:42:41 -0500

    completed exercise

--- server.js
+++ server.js
@@ -27,8 +27,27 @@ Character.belongsTo(Life);
 Life.hasMany(Character);
 
 const express = require('express');
+
 const app = express();
+const methodOverride = require('method-override');
+
+app.use(express.static('public'))
+
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => res.redirect('/marvel'));
+
+app.delete('/marvel/:id', async(req, res, next) => {
+    try {
+        const char = await Character.findByPk(req.params.id);
+        await char.destroy();
+        res.redirect(`/marvel/${char.lifeId}`)
+    }
+    catch(ex){
+        next(ex);
+    }
+});
 
 app.post('/marvel', async(req, res, next) => {
     try {
@@ -55,9 +74,8 @@ app.get('/marvel', async(req, res, next) => {
         }).join('')
         const html = characters.map (character => {
             return `
-            <div>
-                ${character.name} is 
-                <a href='/marvel/${character.life.id}'>${character.life.name}</a>
+            <div class='para'>
+                ${character.name} is<a href='/marvel/${character.life.id}'>${character.life.name}</a>
             </div>
             `
         }).join('')     
@@ -65,6 +83,7 @@ app.get('/marvel', async(req, res, next) => {
         <html>
         <head>
             <title>Marvel Character's Lives</title>
+            <link rel='stylesheet' href='/styles.css' />
         </head>
         <body>
             <h1>Marvel Characters</h1>
@@ -92,20 +111,24 @@ app.get('/marvel/:id', async(req, res, next) => {
         });
         const html = lives.characters.map (life => {
             return `
-            <div>
+            <div class='life'>
                 ${life.name}
+                <form method='POST' action='/marvel/${life.id}?_method=delete'>
+                <button>Erase</button>
+                </form>
             </div>
             `
         }).join('');
         res.send(`
         <html>
         <head>
-        <title>Marvel Characters</title>
+            <title>Marvel Characters</title>
+            <link rel='stylesheet' href='/styles.css' />
         </head>
         <body>
         <h1>Marvel Characters That Are ${lives.name.charAt(0).toUpperCase() + lives.name.slice(1)}</h1>
         <h2>
-        <a href='/marvel'>Back to More Characters!</a>
+        <a href='/marvel'> <-Back to More Characters</a>
         </h2>
         ${html}
         </body>
@@ -121,8 +144,8 @@ const init = async() => {
     try {
         await sequelize.sync({ force: true})
         console.log('starting!')
-        const alive = await Life.create({ name: 'alive'});
-        const decease = await Life.create({ name: 'deceased'});
+        const alive = await Life.create({ name: 'alive 💪'});
+        const decease = await Life.create({ name: 'deceased 💀'});
         await Character.create({ name: 'Iron Man', lifeId: decease.id});
         await Character.create({ name: 'Pietro Maximoff', lifeId: decease.id});
         await Character.create({ name: 'Natasha Romanoff', lifeId: decease.id});
